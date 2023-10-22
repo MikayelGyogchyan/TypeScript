289daa5ea8e63f7e0a3c6d8cdcd0c99cbc383d28
Mikayel <gyogchyanmikayel@gmail.com>
2023-10-22 19:10:31 +0000

    Add files via upload
    Working with Tuples

--- TypeScript/Understanding TypeScrip/TypeScript Basics & Basic Types/app.ts
+++ TypeScript/Understanding TypeScrip/TypeScript Basics & Basic Types/app.ts
@@ -2,20 +2,31 @@
 const person = {
   name: 'Mikayel',
   age: 24,
-  hobbies: ['Sports', 'Cooking'] // hobbies: string[]
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'] // role: (string | number)[]
 }
 
-// console.log(person.name);
+person.role.push('admin')
+person.role[1] = (10)
 
-let favoriteActivities: string[] 
-favoriteActivities = ['Sports']
+// Tuple
 
-let favoriteActivitiesAny: any[] 
-favoriteActivitiesAny = ['Sports', 1]
-
-for(const hobby of person.hobbies) {
-  console.log(hobby); // Sports Cooking
-  console.log(hobby.toUpperCase()); // SPORTS COOKING
+const person2: {
+  name: string,
+  age: number,
+  hobbies: string[],
+  role: [number, string] // Tuple
+} = {
+  name: 'Mikayel',
+  age: 24,
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'] 
 }
 
+// person2.role[1] = (10) // error //  Type 'number' is not assignable to type 'string'
+// person2.role.push('admin') // this will work. Push is an exception. TS can't catch this error
+// person2.role = [0, 'admin', 'user'] // error
 
+/*
+The length is enforced if we assign like this, but not for a pushing.
+*/
