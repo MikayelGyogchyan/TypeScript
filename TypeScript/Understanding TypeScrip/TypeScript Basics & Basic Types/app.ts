89bbc7c626968ad3cea328c65cf72880854bb8ff
Mikayel <gyogchyanmikayel@gmail.com>
2023-10-22 19:57:21 +0000

    Add files via upload
    The "any" Type

--- TypeScript/Understanding TypeScrip/TypeScript Basics & Basic Types/app.ts
+++ TypeScript/Understanding TypeScrip/TypeScript Basics & Basic Types/app.ts
@@ -1,26 +1,6 @@
-// Enums
-
-const ADMIN = 0
-const READ_ONLY = 1
-const AUTHOR = 2
-
-const person = {
-  name: "Mikayel",
-  age: 24,
-  hobbies: ["Sports", "Cooking"],
-  role: ADMIN
-};
-
-if(person.role === ADMIN) { // role: number
-  console.log('Is ADMIN'); // Is ADMIN'
-}
-
-// With Enums
+// any
 
 enum Role { ADMIN, READ_ONLY, AUTHOR} // default //  Role.ADMIN = 0, Role.READ_ONLY = 1, Role.AUTHOR = 2
-enum Role1 { ADMIN = 5, READ_ONLY, AUTHOR} // Role.ADMIN = 5, Role.READ_ONLY = 6, Role.AUTHOR = 7
-enum Role2 { ADMIN = 5, READ_ONLY, AUTHOR = 33} // Role.ADMIN = 5, Role.READ_ONLY = 6, Role.AUTHOR = 33
-enum Role3 { ADMIN = 'ADMIN', READ_ONLY = 44, AUTHOR = 33} // Role.ADMIN = "ADMIN", Role.READ_ONLY = 44, Role.AUTHOR = 33
 
 const person2 = {
   name: "Mikayel",
@@ -33,3 +13,10 @@ if(person2.role === Role.ADMIN) { // role: number
   console.log('Is ADMIN'); // Is ADMIN'
 }
 
+let favoriteActivities: any[] // array of something
+favoriteActivities = ['Sports', 5]
+
+/*
+We can use any as a fallback, where we have same kind of data where we 
+really can't know which kind of data will be stored in there.
+*/
