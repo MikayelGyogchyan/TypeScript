4daf866b2e0fae787940c11ac8df93d9deace06f
Mikayel <gyogchyanmikayel@gmail.com>
2023-10-22 20:25:01 +0000

    Add files via upload
    The "Any" Types

---
+++ TypeScript/Understanding TypeScrip/TypeScript Basics & Basic Types/core-types/any.ts
@@ -0,0 +1,9 @@
+// any
+
+let favoriteActivities: any[] // array of something
+favoriteActivities = ['Sports', 5]
+
+/*
+We can use any as a fallback, where we have same kind of data where we 
+really can't know which kind of data will be stored in there.
+*/
