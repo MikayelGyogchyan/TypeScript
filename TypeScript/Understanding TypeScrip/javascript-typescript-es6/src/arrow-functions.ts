653ec93346e23c4febf75b6e928bda2c6ab3cfd6
Mikayel <gyogchyanmikayel@gmail.com>
2023-11-02 20:49:59 -0100

    Add files via upload
    arrow function and typescript

---
+++ TypeScript/Understanding TypeScrip/javascript-typescript-es6/src/arrow-functions.ts
@@ -0,0 +1,16 @@
+// const add = (a: number, b: number) => a + b;
+// console.log(add(2, 5));
+
+// const printOutput = (output: string | number) => console.log(output);
+// // const printOutput = output: string | number => console.log(output);
+// // const printOutput = output => console.log(output);
+// const printOutput2: (a:number | string) => void = output => console.log(output);
+
+// const button = document.querySelector('button');
+
+// if(button) {
+//   button.addEventListener('click', event => console.log(event))
+// }
+
+// printOutput(add(5, 2))
+
